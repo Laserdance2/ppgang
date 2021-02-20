701df1800267aa046d0424be9ab9d2f3b300fc49
maxhayes2003 <78435236+maxhayes2003@users.noreply.github.com>
2021-02-20 13:23:25 +0100

    Add files via upload

---
+++ Assets/Scripts/healthbarVar.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+public class healthbarVar : MonoBehaviour
+{
+    public int healthbarVarCounter;
+    public Text healthText;
+void Start(){
+
+GameObject Player = GameObject.Find("Player");
+playerLives playerLives = Player.GetComponent<playerLives>();
+
+healthbarVarCounter = playerLives.playerLiveCounter;
+
+}
+
+    
+    void Update()
+    {
+        healthText.text = "Health: " + healthbarVarCounter.ToString();
+    }
+}
---
+++ Assets/Scripts/playerLives.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerLives : MonoBehaviour
+{
+    public int playerLiveCounter;
+    void Start()
+    {
+        playerLiveCounter = 100;
+    }
+
+    
+    public void TakeDamage()
+    {
+        //hier moet de collision + levens eraf komen maar deze functies moeten nog gedefinieerd worden
+        if(playerLiveCounter <= 0){
+            Die();
+        }
+        }
+    
+
+void Die(){
+    Debug.Log("Player died");
+
+    this.enabled = false;
+    GetComponent<Collider2D>().enabled = false;
+    }
+}
