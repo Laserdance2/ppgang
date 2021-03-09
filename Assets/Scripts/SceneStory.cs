16ba4de16964cf6d46a52e0b2f1ab214e4301e3d
Stefan <scoartastefan@gmail.com>
2021-03-09 18:48:18 +0100

    Zorgen dat StoryScene werkt nu, HealthBar toegevoegd

---
+++ Assets/Scripts/SceneStory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneStory : MonoBehaviour{
+
+
+   public void Continue(){
+ 		SceneManager.LoadScene("TestLevel");
+ 	}
+
+}
--- Assets/Scripts/healthbarVar.cs
+++ Assets/Scripts/healthbarVar.cs
@@ -1,25 +1,33 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthbarVar : MonoBehaviour
 {
-    public int healthbarVarCounter;
-    public Text healthText;
+   // public int healthbarVarCounter;
+   //public Text healthText;
+    public Slider slider;
+    public PlayerLives playerLives;
+    public GameObject player;
+
     
     void Start()
     {
 
-        GameObject Player = GameObject.Find("Player");
-        PlayerLives playerLives = Player.GetComponent<PlayerLives>();
+        
+        playerLives = player.GetComponent<PlayerLives>();
 
-        healthbarVarCounter = playerLives.playerLiveCounter;
+        //healthbarVarCounter = playerLives.playerLiveCounter;
+        slider.value = playerLives.playerLiveCounter;
+        Debug.Log(playerLives.playerLiveCounter);
 
     }
 
     
     void Update()
     {
-        healthText.text = "Health: " + healthbarVarCounter.ToString();
+       // healthText.text = "Health: " + healthbarVarCounter.ToString();
+        slider.value = playerLives.playerLiveCounter;
     }
 }
