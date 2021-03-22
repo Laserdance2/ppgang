567f02ad08aa8df6a1c1e5029b8b0b690602c127
Max <maxhayes2003@gmail.com>
2021-03-22 18:17:50 +0100

    enemy schieten scripts, bullet script

---
+++ Assets/Enemy_Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Weapon : MonoBehaviour
+{
+    
+public Transform firePoint;
+public GameObject bulletPrefab;
+
+
+    void Update()
+    {
+        if(GameObject.Find("chinese").GetComponent<Enemy_Shoot>().seePlayer){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+    }
+}
