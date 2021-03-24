2c2e062e818d64a9a39d99133904c0c15b305835
Jared from Subway <stephen.vanelk@gmail.com>
2021-03-24 14:18:04 +0100

    F

--- Assets/Scripts/ShopMenu.cs
+++ Assets/Scripts/ShopMenu.cs
@@ -15,8 +15,13 @@ public static class ButtonExtension
     }
 
 }
+
+
+
 public class ShopMenu : MonoBehaviour
 {
+    GameObject buttonTemplate;
+
     [Serializable]
     public struct Item
     {
@@ -24,7 +29,45 @@ public class ShopMenu : MonoBehaviour
         public string Desc;
         public int Price;
         public Sprite Icon;
+        public int itemID;
+        public bool isPurchased;
 
+        public Item(String name, String desc, int price, Sprite icon, int itemid)
+        {
+            Name = name;
+            Desc = desc;
+            Price = price;
+            Icon = icon;
+            itemID = itemid;
+            isPurchased = false;
+        }
+
+        public String getName(int ID)
+        {
+            return null;
+        }
+
+        public String getDesc(int ID)
+        {
+            return null;
+        }
+
+        public Sprite getIcon(int ID)
+        {
+            return null;
+        }
+
+        public Item(int price, int itemid)
+        {
+            Name = getName(itemid);
+            Desc = getDesc(itemid);
+            Price = price;
+            Icon = getIcon(itemid);
+            itemID = itemid;
+            isPurchased = false;
+        }
+
+        
     }
 
     [SerializeField] Item[] allItems;
@@ -33,32 +76,82 @@ public class ShopMenu : MonoBehaviour
 
     void Start()
     {
-        GameObject buttonTemplate = transform.GetChild(0).gameObject;
-        GameObject g;
+        buttonTemplate = transform.GetChild(0).gameObject;
 
+        initShop();
+        buttonTemplate.SetActive(false);
+
+    }
+
+    void initShop()
+    {
         int N = allItems.Length;
 
         for (int i = 0; i < N; i++)
         {
-            g = Instantiate(buttonTemplate, transform);
-            g.transform.GetChild(0).GetComponent<Image>().sprite = allItems[i].Icon;
-            g.transform.GetChild(1).GetComponent<Text>().text = allItems[i].Desc;
-            g.transform.GetChild(2).GetComponent<Text>().text = allItems[i].Name;
-            g.transform.GetChild(3).GetComponent<Text>().text = (allItems[i].Price).ToString();
-
-            /*g.GetComponent<Button>().onClick.AddListener(delegate ()
-           {
-               ItemClicked(i);
-           });
-           */
-            g.GetComponent<Button>().AddEventListener(i, ItemClicked);
+            if (allItems[i].isPurchased == false)
+            {
+                createButton(i);
+            }
         }
+        buttonTemplate.SetActive(false);
 
-        Destroy(buttonTemplate);
     }
+
+
+    void buyItem(int i)
+    {
+        //if (player.currency >= item.price)
+        //   {
+        //    player.currency -= item.price;
+        //  inventory.addItem(item);
+        allItems[i].isPurchased = true;
+        //   }
+        refreshShop();
+    }
+
+    void refreshShop()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.transform.GetChild(1).GetComponent<Text>().text != "Template")
+                GameObject.Destroy(child.gameObject);
+        }
+        buttonTemplate.SetActive(true);
+        initShop();
+
+    }
+
+    void createButton(int n)
+    {
+        GameObject g;
+        g = Instantiate(buttonTemplate, transform);
+        g.transform.GetChild(0).GetComponent<Image>().sprite = allItems[n].Icon;
+        g.transform.GetChild(1).GetComponent<Text>().text = allItems[n].Name;
+        g.transform.GetChild(2).GetComponent<Text>().text = allItems[n].Desc;
+        g.transform.GetChild(3).GetComponent<Text>().text = (allItems[n].Price).ToString();
+
+        /*g.GetComponent<Button>().onClick.AddListener(delegate ()
+       {
+           ItemClicked(i);
+       });
+       */
+        g.GetComponent<Button>().AddEventListener(n, ItemClicked);
+    }
+
+
+
+    void addItem(Item item)
+    {
+        allItems.SetValue(item, allItems.Length);
+    }
+
+
 
     void ItemClicked(int itemIndex)
     {
-        Debug.Log("item " + itemIndex + " clicked");
+        buyItem(itemIndex);
     }
+
+
 }
