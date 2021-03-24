b378ceb932f8214dcef47062addca6a56a2b3ac1
Max <maxhayes2003@gmail.com>
2021-03-24 16:15:51 +0100

    enemy shoot 1 nog net niet klaar

--- Assets/Enemy_Weapon.cs
+++ Assets/Enemy_Weapon.cs
@@ -6,16 +6,43 @@ public class Enemy_Weapon : MonoBehaviour
 {
     public Transform firePointChinese;
     public GameObject BulletPrefab;
+    public float shootCooldown;
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    public bool canShoot;
+
+    void Start(){
+    shootCooldown = 0;
+    spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        if(GameObject.Find("chinese").GetComponent<Enemy_Shoot>().seePlayer){
-            Shoot();
+        shootCooldown += Time.deltaTime;
+
+        if(shootCooldown >= 1){
+            canShoot = true;
         }
+        else{
+            canShoot = false;
+        }
+
+        if((GameObject.Find("chinese").GetComponent<Enemy_Shoot>().seePlayer) && (canShoot)){
+            Shoot();
+            shootCooldown = 0;
+            canShoot = false;
+        }
+
+        
+        
     }
 
+    void ChangeSprite(){
+        spriteRenderer.sprite = newSprite;
+    }
     void Shoot()
     {
+        ChangeSprite();
         Instantiate(BulletPrefab, firePointChinese.position, firePointChinese.rotation);
     }
 }
